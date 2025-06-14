b6e92a9a716eff0e5af845031bb89caf72027080
VedhaVishnu-N <nvedhavishnu@gmail.com>
2025-06-14 18:31:17 +0530

    Update RemoveDuplicatesElements.py
    modified version of Removing Duplicate Element from sorted array with reduced Complexity

--- RemoveDuplicatesElements.py
+++ RemoveDuplicatesElements.py
@@ -9,3 +9,15 @@ class Solution(object):
             arr.append(nums[j])
         return i
         
+# Here is an modified version of Removing Duplicate Element from sorted array with reduced Complexity 
+class Solution(object):
+    def removeDuplicates(self, nums):
+        i=0
+        val=None
+        for j in range(len(nums)):
+            if nums[j]!=val:
+                nums[i]=nums[j]
+                i+=1
+            val=nums[j]
+        return i
+        
